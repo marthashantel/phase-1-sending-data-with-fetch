9ff926920f342ae746fae9e42ab4587b40d54f30
marthashantel <shantelmartha112@gmail.com>
2025-06-26 10:31:16 +0300

    add

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+function submitData(userName, userEmail) {
+  const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+    },
+    body: JSON.stringify({
+      name: userName,
+      email: userEmail,
+    }),
+  };
+
+  return fetch("http://localhost:3000/users", configurationObject)
+    .then(response => response.json())
+    .then(object => {
+      const idElement = document.createElement("p");
+      idElement.textContent = object.id;
+      document.body.appendChild(idElement);
+    })
+    .catch(error => {
+      const errorElement = document.createElement("p");
+      errorElement.textContent = error.message;
+      document.body.appendChild(errorElement);
+    });
+}
